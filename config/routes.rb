501faa525f94769bc507d20dcc92254f73eda268
Mirek <mirek@szul.eu>
2018-09-24 01:21:29 +0200

    Change routes for contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/contact'
-  # get 'visits/index'
-  # get 'visits/show'
-  # get 'visits/new'
-  # get 'visits/edit'
-  # get 'visits/create'
-  # get 'visits/update'
-  # get 'visits/destroy'
-  # get 'patients/index'
-  # get 'patients/show'
-  # get 'patients/new'
-  # get 'patients/edit'
-  # get 'patients/create'
-  # get 'patients/update'
-  # get 'patients/destroy'
-  # get 'doctors/index'
-  # get 'doctors/show'
-  # get 'doctors/new'
-  # get 'doctors/edit'
-  # get 'doctors/create'
-  # get 'doctors/update'
-  # get 'doctors/destroy'
-  # get 'doctor/index'
-  # get 'doctor/show'
-  # get 'doctor/new'
-  # get 'doctor/edit'
-  # get 'doctor/create'
-  # get 'doctor/update'
-  # get 'doctor/destroy'
-
   root 'static_pages#home'
+  # get 'static_pages/contact'
+  get '/contact',    to: 'static_pages#contact', as: 'contact'
+
   resources :patients
   resources :doctors
   resources :visits
