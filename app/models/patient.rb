2664676164390e846a862ca11bba9f7655bfffae
Mirek <mirek@szul.eu>
2018-11-13 18:15:45 +0100

    Configure paperclip for patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,6 +4,9 @@ class Patient < ApplicationRecord
   has_many :visits
   has_many :doctors, through: :visits
 
+  has_attached_file :avatar_patient, styles: { medium: "300x300>", thumb: "140x140>" }, default_url: "/images/:style/avatar.gif"
+  validates_attachment_content_type :avatar_patient, content_type: /\Aimage\/.*\z/
+
   def age_of_patient
     Time.now.year - date_of_birth.year
   end
