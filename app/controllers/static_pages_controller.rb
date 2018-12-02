07e68511c17bf0604769d7a6e615ba3c1987153d
Mirek <mirek@szul.eu>
2018-12-02 23:01:58 +0100

    Add sorting by date to the visit table

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @visits = Visit.all.order('start_visit ASC')
+  end
 
   def contact; end
 end
