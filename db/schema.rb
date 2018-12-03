744c0231d9eff4836e2af98507e29f200d4d4aa8
Mirek <mirek@szul.eu>
2018-12-03 15:51:22 +0100

    Remove age column from patients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_170059) do
+ActiveRecord::Schema.define(version: 2018_12_03_144758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2018_11_13_170059) do
   create_table "patients", force: :cascade do |t|
     t.string "name"
     t.string "surname"
-    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "date_of_birth"
