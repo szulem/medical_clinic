f9c421749e679cd00f39ef5e3d80f0504d733e87
Mirek <mirek@szul.eu>
2018-12-04 14:08:59 +0100

    Add validations for doctor's name and surname

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,8 @@
 class Doctor < ApplicationRecord
   has_many :visits
   has_many :patients, through: :visits
+
+  validates :name, presence: { message: "Name should have min. 2 characters!" }, length: { minimum: 2, maximum: 30 }
+  validates :surname, presence: { message: "Surname should have min. 2 characters!" }, length: { minimum: 2, maximum: 30 }
+
 end
