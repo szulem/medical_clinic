9807d06fefdad60e6275e9ff86a3dd850373677b
Mirek <mirek@szul.eu>
2018-12-05 18:12:31 +0100

    Add callback titleize_doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class Doctor < ApplicationRecord
+  before_create :titleize_doctor
+
   has_many :visits
   has_many :patients, through: :visits
 
   validates :name, presence: { message: "Name should have min. 2 characters!" }, length: { minimum: 2, maximum: 30 }
   validates :surname, presence: { message: "Surname should have min. 2 characters!" }, length: { minimum: 2, maximum: 30 }
+
+  private
+
+  def titleize_doctor
+    self.name = self.name.to_s.titleize
+    self.surname = self.surname.to_s.titleize
+  end
 
 end
